d76de9638d9013eb7b7241acb0a21574a0d8f600
Atom <jintong.liu@outlook.com>
2025-03-30 23:15:32 +0800

    Complete computeCost

--- assignments/ex1/computeCost.m
+++ assignments/ex1/computeCost.m
@@ -6,16 +6,21 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+#for i = 1:m
+#  h = theta(1, 1) + theta(2, 1) * X(i, 2);
+#  J = J + (h - y(i, 1))^2;
+#end
 
+#J = J / (2 * m);
 
-
+J = sum((X * theta - y).^2) / (2 * m);
 
 % =========================================================================
 
